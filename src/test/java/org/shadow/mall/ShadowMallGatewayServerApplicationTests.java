da33e0e6106ada24cafc88a1202f9f65bb8ac038
jianjustin <janine.jian.chen@gmail.com>
2020-05-28 22:00:39 +0800

    初始化网关服务

---
+++ src/test/java/org/shadow/mall/ShadowMallGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shadow.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowMallGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
